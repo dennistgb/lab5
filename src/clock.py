c76f47ac3117cfebc623379f1a0ba1b12a2d0cf6
Dennis <dennistgb579@gmail.com>
2024-05-16 12:56:36 +0800

    added in safe

--- src/clock.py
+++ src/clock.py
@@ -1,16 +1,21 @@
 import time 
 from hal import hal_lcd as LCD
+from threading import Thread
 
 def clock_thread():
-    local_time = time.localtime() # get struct_time 
+    display = LCD.lcd()
     global time_string
+    local_time = time.localtime() # get struct_time     
     time_string = time.strftime("%H:%M:%S", local_time) 
+    print(time_string)
+    display.lcd_display_string(f"timeis: {time_string}",1)
 
 def main():
     display = LCD.lcd()
     display.lcd_clear()
-    while(True):
-        display.lcd_display_string(time_string,1)
-        display.lcd_display_string("dd:mm:yyyy",2)
+    while(1):
+        clock__thread = Thread(target=clock_thread())
+        time.sleep(0.792)
 
-main()
+if __name__ == "__main__":
+    main()
